6724d63eb1b867448c3de21e8395f01ded17701c
oscar <oscarintel42@gmail.com>
2021-03-24 19:52:00 +0200

    03.24 Commit

---
+++ requests_example.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import requests
+
+# pip install requests
+response1 = requests.get('http://example.com')
+response2 = requests.put('http://example.com')
+response3 = requests.post('http://example.com')
+response4 = requests.delete('http://example.com')
